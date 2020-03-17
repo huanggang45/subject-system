9ecdc19b27b4ea835c9f52cbfe6939d124312a23
huanggang <huanggang45@163.com>
2020-03-17 08:14:32 +0800

    second commit

--- src/identifier.py
+++ src/identifier.py
@@ -7,6 +7,8 @@ import pickle
 
 class Nid:
     def __init__(self, role, db_path):
-        role_list = [
-            'admin', 'school', 'teacher', 'course', 'course_to_teacher', 'classes', ''
-        ]
+        role_list = ['admin', 'school', 'teacher', 'course', 'course_to_teacher', 'classes', 'student']
+
+    if role not in role_list:
+        raise Exception("用户角色错误，选项：%s" % ','.join(role_list))
+    self.role = role
